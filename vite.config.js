f99d4ec41e3514d8e0d4b9423029f0071358dbdd
Sebastian <sebastiantorkowski@gmail.com>
2023-12-23 11:31:20 +0100

    api change

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://www.coingecko.com/api/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    host: '127.0.0.1'
   },
   plugins: [react()],
-  
  
 })
