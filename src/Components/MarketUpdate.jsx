a225ba4bd860d19c6a48932f3744227d5fe9675c
Sebastian <sebastiantorkowski@gmail.com>
2023-12-23 07:13:00 +0100

    fixes

--- src/Components/MarketUpdate.jsx
+++ src/Components/MarketUpdate.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "ldrs/bouncy";
 
 function MarketUpdate({ data, formatAsPercent, currencyFormat }) {
   const [scrollPosition, setScrollPosition] = useState(0);
@@ -45,46 +46,52 @@ function MarketUpdate({ data, formatAsPercent, currencyFormat }) {
             <th>Market Cap</th>
           </tr>
         </thead>
-        <tbody>
-          {data &&
-            data.slice(0, 8).map((coin, index) => {
-              return (
-                <tr
-                  key={index}
-                  style={{ cursor: showMarketContainer && "pointer" }}
-                  className="coin-data-container"
-                >
-                  <td className="position-num">{index + 1}</td>
-                  <td>
-                    <div className="coin-logo-name-wrapper">
-                      <img src={coin.image} />
-                      <h2 className="coin-name">
-                        {coin.name} <span>{coin.symbol.toUpperCase()}</span>
-                      </h2>
-                    </div>
-                  </td>
-                  <td className="current-price">
-                    {currencyFormat(coin.current_price)}
-                  </td>
-                  <td
-                    className="coin-precentage-change"
-                    style={{
-                      color: coin.price_change_percentage_24h
-                        .toString()
-                        .includes("-")
-                        ? "hsl(0, 64%, 52%)"
-                        : "hsl(142, 43%, 54%)",
-                    }}
+        {data ? (
+          <tbody>
+            {data &&
+              data.slice(0, 8).map((coin, index) => {
+                return (
+                  <tr
+                    key={index}
+                    style={{ cursor: showMarketContainer && "pointer" }}
+                    className="coin-data-container"
                   >
-                    {formatAsPercent(coin.price_change_percentage_24h)}
-                  </td>
-                  <td className="market-cap">
-                    {currencyFormat(coin.market_cap)}
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
+                    <td className="position-num">{index + 1}</td>
+                    <td>
+                      <div className="coin-logo-name-wrapper">
+                        <img src={coin.image} />
+                        <h2 className="coin-name">
+                          {coin.name} <span>{coin.symbol.toUpperCase()}</span>
+                        </h2>
+                      </div>
+                    </td>
+                    <td className="current-price">
+                      {currencyFormat(coin.current_price)}
+                    </td>
+                    <td
+                      className="coin-precentage-change"
+                      style={{
+                        color: coin.price_change_percentage_24h
+                          .toString()
+                          .includes("-")
+                          ? "hsl(0, 64%, 52%)"
+                          : "hsl(142, 43%, 54%)",
+                      }}
+                    >
+                      {formatAsPercent(coin.price_change_percentage_24h)}
+                    </td>
+                    <td className="market-cap">
+                      {currencyFormat(coin.market_cap)}
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        ) : (
+          <div className="loading-container">
+            <l-bouncy size="45" speed="1.75" color="black"></l-bouncy>
+          </div>
+        )}
       </table>
     </div>
   );
